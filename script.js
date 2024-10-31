67fa0e50efc69cb7cf0f6a3569c9336e96c2dc85
Priyansh Dimri <priyanshd0908@gmail.com>
2024-10-31 12:20:52 +0530

    feat: add addTileRow function to add tile div's
    - Added addTileRow function to add given number of tiles in a row and
    adding it to the gameArea element.
    - Added style to tile-row and tile and game-area to create uniform
      sketch grid

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+// Creates a row containing squaresPerSide div's of class 'tile'
+function addTileRow(gameArea, squaresPerSide) {
+    const tileRow = document.createElement('div');
+    tileRow.classList.add('tile-row');
+
+    for(let i = 0; i < squaresPerSide; ++i) {
+        let tile = document.createElement('div');
+        tile.classList.add('tile');
+        tileRow.appendChild(tile);
+    }
+
+    gameArea.appendChild(tileRow);
+}
+
+// Creates squaresPerSide X squaresPerSide grid using flex box elements.
 function createSketchPad(squaresPerSide) {
     const gameArea = document.querySelector('#game-area');
 
-    /* 
-     * Creates squaresPerSide X squaresPerSide grid using flex box elements.
-     */
+    for(let i = 0; i < squaresPerSide; ++i) {
+        addTileRow(gameArea, squaresPerSide);
+    }
 }
+
+// Default 
+createSketchPad(16);
